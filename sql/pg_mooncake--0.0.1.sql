1f75c6f6f10f044816b8b646a394a04d2b7d6a20
Zhou Sun <zhou@mooncakelabs.com>
2024-10-07 11:11:26 -0700

    Update pg_mooncake--0.0.1.sql

--- sql/pg_mooncake--0.0.1.sql
+++ sql/pg_mooncake--0.0.1.sql
@@ -1,15 +1,15 @@
 CREATE SCHEMA mooncake;
 
-CREATE TABLE mooncake.data_files (
-    relid OID NOT NULL,
-    file_name TEXT NOT NULL
-);
-
 CREATE TABLE mooncake.table_info (
     relid OID NOT NULL primary key,
     storage_path TEXT NOT NULL,
     lake_format TEXT NOT NULL,
     lakehouse_options JSONB
+);
+
+CREATE TABLE mooncake.data_files (
+    relid OID NOT NULL,
+    file_name TEXT NOT NULL
 );
 
 CREATE FUNCTION columnstore_handler(internal) RETURNS table_am_handler

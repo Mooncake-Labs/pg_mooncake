27a9c5252ed62c9cde27ed33191b60052bb32f94
dentiny <dentinyhao@gmail.com>
2024-11-19 07:30:32 +0000

    cpp improvement for columnstore classes

--- src/lake/lake.cpp
+++ src/lake/lake.cpp
@@ -2,7 +2,11 @@
 #include "duckdb/common/unordered_set.hpp"
 #include "rust_extensions/delta.hpp"
 
+#include <utility>
+
 namespace duckdb {
+
+namespace {
 
 class LakeWriter {
 public:
@@ -27,11 +31,12 @@ public:
             cached_table_infos[oid] = {path, metadata.SecretsSearchDeltaOptions(path)};
         }
         auto &files = xact_state[oid];
-        if (files.count(file_name) == 0) {
-            files[file_name] = {file_size, is_add_file};
+        auto files_iter = files.find(file_name);
+        if (files_iter == files.end()) {
+            files.emplace(std::move(file_name), FileInfo{file_size, is_add_file});
         } else {
-            D_ASSERT(files[file_name].is_add_file && !is_add_file);
-            files.erase(file_name);
+            D_ASSERT(files_iter->second.is_add_file && !is_add_file);
+            files.erase(files_iter);
         }
     }
 
@@ -45,12 +50,15 @@ public:
         }
         for (auto &[oid, files] : xact_state) {
             vector<string> file_names;
+            file_names.reserve(files.size());
             vector<int64_t> file_sizes;
+            file_sizes.reserve(files.size());
             vector<int8_t> is_add_files;
-            for (auto &[file_name, file_info] : files) {
-                file_names.push_back(file_name);
-                file_sizes.push_back(file_info.file_size);
-                is_add_files.push_back(file_info.is_add_file);
+            is_add_files.reserve(files.size());
+            for (const auto &[file_name, file_info] : files) {
+                file_names.emplace_back(file_name);
+                file_sizes.emplace_back(file_info.file_size);
+                is_add_files.emplace_back(file_info.is_add_file);
             }
             if (file_names.size()) {
                 auto info = cached_table_infos[oid];
@@ -74,7 +82,11 @@ private:
     unordered_map<Oid, unordered_map<string, FileInfo>> xact_state;
 };
 
+// TODO(mooncake): Make `lake_writer` thread-safe. For the current implementation, concurrent table creation is not
+// guarded by mutex.
 LakeWriter lake_writer;
+
+} // namespace
 
 void LakeCreateTable(Oid oid, const string &path) {
     lake_writer.CreateTable(oid, path);

Mooncake-Labs/pg_mooncake

commit 6d34be55fc0a724a5fccf1a0c38e1f167b6a30fe

Basic support for special columns
1. Support CONST as column default value
2. Support identity column
3. Lockdown generated column

src/pgduckdb/catalog/pgduckdb_table.cpp
@@ -2,6 +2,8 @@
 #include "pgduckdb/pgduckdb_process_lock.hpp"
 #include "pgduckdb/catalog/pgduckdb_schema.hpp"
 #include "pgduckdb/catalog/pgduckdb_table.hpp"
+#include "duckdb/parser/expression/constant_expression.hpp"
+#include "duckdb/parser/expression/function_expression.hpp"
 #include "duckdb/parser/parsed_data/create_table_info.hpp"
 #include "pgduckdb/scan/postgres_seq_scan.hpp"
 #include "pgduckdb/scan/postgres_scan.hpp"
@@ -11,6 +13,7 @@ extern "C" {
 #include "postgres.h"
 #include "access/tableam.h"
 #include "access/heapam.h"
+#include "catalog/dependency.h"
 #include "storage/bufmgr.h"
 #include "catalog/namespace.h"
 #include "catalog/pg_class.h"
@@ -46,13 +49,32 @@ PostgresTable::OpenRelation(Oid relid) {
 
 void
 PostgresTable::SetTableInfo(CreateTableInfo &info, ::Relation rel) {
-	auto tupleDesc = RelationGetDescr(rel);
+	auto desc = RelationGetDescr(rel);
 
-	for (int i = 0; i < tupleDesc->natts; ++i) {
-		Form_pg_attribute attr = &tupleDesc->attrs[i];
+	for (int i = 0; i < desc->natts; ++i) {
+		Form_pg_attribute attr = &desc->attrs[i];
 		auto col_name = duckdb::string(NameStr(attr->attname));
 		auto duck_type = pgduckdb::ConvertPostgresToDuckColumnType(attr);
-		info.columns.AddColumn(duckdb::ColumnDefinition(col_name, duck_type));
+		ColumnDefinition column(col_name, duck_type);
+		if (attr->atthasdef) {
+			Node *node = TupleDescGetDefault(desc, i + 1);
+			if (!IsA(node, Const)) {
+				elog(ERROR, "column \"%s\" has unsupported default value ", NameStr(attr->attname));
+			}
+			Const *val = castNode(Const, node);
+			if (val->constisnull) {
+				column.SetDefaultValue(make_uniq<ConstantExpression>(Value(duck_type)));
+			} else {
+				column.SetDefaultValue(make_uniq<ConstantExpression>(
+				    pgduckdb::ConvertPostgresParameterToDuckValue(val->constvalue, val->consttype)));
+			}
+		} else if (attr->attidentity) {
+			Oid seqid = getIdentitySequence(rel, i + 1, false /*missing_ok*/);
+			vector<unique_ptr<ParsedExpression>> children;
+			children.push_back(make_uniq<ConstantExpression>(Value::UINTEGER(seqid)));
+			column.SetDefaultValue(make_uniq<FunctionExpression>("pg_nextval", std::move(children)));
+		}
+		info.columns.AddColumn(std::move(column));
 		/* Log column name and type */
 		elog(DEBUG2, "(DuckDB/SetTableInfo) Column name: %s, Type: %s --", col_name.c_str(),
 		     duck_type.ToString().c_str());

src/columnstore_handler.cpp
@@ -137,6 +137,9 @@ void columnstore_relation_set_new_filenode(Relation rel, const RelFileNode *newr
             if (duck_type.id() == duckdb::LogicalTypeId::USER) {
                 elog(ERROR, "column \"%s\" has unsupported type", NameStr(attr->attname));
             }
+            if (attr->attgenerated) {
+                elog(ERROR, "unsupported generated column \"%s\"", NameStr(attr->attname));
+            }
         }
 
         duckdb::Columnstore::CreateTable(rel->rd_id);

src/pgduckdb/pgduckdb_duckdb.cpp
@@ -4,10 +4,12 @@
 #include "duckdb/main/client_data.hpp"
 #include "duckdb/catalog/catalog_search_path.hpp"
 #include "duckdb/main/extension_install_info.hpp"
+#include "duckdb/parser/parsed_data/create_scalar_function_info.hpp"
 
 extern "C" {
 #include "postgres.h"
 #include "catalog/namespace.h"
+#include "commands/sequence.h"
 #include "utils/lsyscache.h"
 #include "utils/fmgrprotos.h"
 }
@@ -20,6 +22,31 @@ extern "C" {
 #include "pgduckdb/scan/postgres_seq_scan.hpp"
 #include "pgduckdb/pgduckdb_utils.hpp"
 #include "pgduckdb/catalog/pgduckdb_storage.hpp"
+
+namespace duckdb {
+
+static void
+PgNextval(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &input = args.data[0];
+	D_ASSERT(input.GetVectorType() == VectorType::CONSTANT_VECTOR);
+	Oid seqid = *ConstantVector::GetData<Oid>(input);
+
+	result.SetVectorType(VectorType::FLAT_VECTOR);
+	auto result_data = FlatVector::GetData<int64_t>(result);
+	for (idx_t i = 0; i < args.size(); i++) {
+		result_data[i] = pgduckdb::PostgresFunctionGuard<int64_t>(nextval_internal, seqid, false /*check_permissions*/);
+	}
+}
+
+static void
+LoadPgNextval(ClientContext &context) {
+	ScalarFunction pg_nextval("pg_nextval", {LogicalType::UINTEGER}, LogicalType::BIGINT, PgNextval);
+	pg_nextval.stability = FunctionStability::VOLATILE;
+	CreateScalarFunctionInfo info(std::move(pg_nextval));
+	context.RegisterFunction(info);
+}
+
+} // namespace duckdb
 
 namespace pgduckdb {
 
@@ -104,6 +131,7 @@ DuckDBManager::Initialize() {
 		                             "SET motherduck_background_catalog_refresh_inactivity_timeout='99 years'");
 	}
 
+	duckdb::LoadPgNextval(context);
 	LoadFunctions(context);
 	// LoadExtensions(context);
 }

src/pgduckdb/vendor/pg_ruleutils_15.c
@@ -6708,6 +6708,9 @@ get_insert_query_def(Query *query, deparse_context *context,
 		if (tle->resjunk)
 			continue;			/* ignore junk entries */
 
+		if (IsA(tle->expr, NextValueExpr))
+			continue;
+
 		appendStringInfoString(buf, sep);
 		sep = ", ";
 

src/pgduckdb/vendor/pg_ruleutils_16.c
@@ -6668,6 +6668,9 @@ get_insert_query_def(Query *query, deparse_context *context,
 		if (tle->resjunk)
 			continue;			/* ignore junk entries */
 
+		if (IsA(tle->expr, NextValueExpr))
+			continue;
+
 		appendStringInfoString(buf, sep);
 		sep = ", ";
 

src/pgduckdb/vendor/pg_ruleutils_17.c
@@ -6682,6 +6682,9 @@ get_insert_query_def(Query *query, deparse_context *context,
 		if (tle->resjunk)
 			continue;			/* ignore junk entries */
 
+		if (IsA(tle->expr, NextValueExpr))
+			continue;
+
 		appendStringInfoString(buf, sep);
 		sep = ", ";
 

test/expected/columns.out
@@ -1,0 +1,49 @@
+-- UNDONE: https://github.com/duckdb/pg_duckdb/issues/445
+CREATE TABLE t (a int DEFAULT NULL, b int) USING columnstore;
+INSERT INTO t VALUES (DEFAULT, 1), (DEFAULT, 2);
+INSERT INTO t VALUES (3, 3), (4, 4);
+INSERT INTO t SELECT 5, 5;
+SELECT * FROM t;
+ a | b 
+---+---
+   | 1
+   | 2
+ 3 | 3
+ 4 | 4
+ 5 | 5
+(5 rows)
+
+DROP TABLE t;
+CREATE TABLE t (a int DEFAULT 3, b int) USING columnstore;
+INSERT INTO t VALUES (DEFAULT, 1), (DEFAULT, 2);
+INSERT INTO t VALUES (3, 3), (4, 4);
+INSERT INTO t SELECT 5, 5;
+SELECT * FROM t;
+ a | b 
+---+---
+ 3 | 1
+ 3 | 2
+ 3 | 3
+ 4 | 4
+ 5 | 5
+(5 rows)
+
+DROP TABLE t;
+CREATE TABLE t (a int DEFAULT 1 + 2, b int) USING columnstore;
+INSERT INTO t (b) VALUES (1), (2);
+ERROR:  column "a" has unsupported default value 
+DROP TABLE t;
+CREATE TABLE t (a int GENERATED ALWAYS AS IDENTITY, b int) USING columnstore;
+INSERT INTO t (b) VALUES (1), (2);
+INSERT INTO t (b) SELECT 3;
+SELECT * FROM t;
+ a | b 
+---+---
+ 1 | 1
+ 2 | 2
+ 3 | 3
+(3 rows)
+
+DROP TABLE t;
+CREATE TABLE t (a int GENERATED ALWAYS AS (b + 1) STORED, b int) USING columnstore;
+ERROR:  unsupported generated column "a"

test/sql/columns.sql
@@ -1,0 +1,27 @@
+-- UNDONE: https://github.com/duckdb/pg_duckdb/issues/445
+
+CREATE TABLE t (a int DEFAULT NULL, b int) USING columnstore;
+INSERT INTO t VALUES (DEFAULT, 1), (DEFAULT, 2);
+INSERT INTO t VALUES (3, 3), (4, 4);
+INSERT INTO t SELECT 5, 5;
+SELECT * FROM t;
+DROP TABLE t;
+
+CREATE TABLE t (a int DEFAULT 3, b int) USING columnstore;
+INSERT INTO t VALUES (DEFAULT, 1), (DEFAULT, 2);
+INSERT INTO t VALUES (3, 3), (4, 4);
+INSERT INTO t SELECT 5, 5;
+SELECT * FROM t;
+DROP TABLE t;
+
+CREATE TABLE t (a int DEFAULT 1 + 2, b int) USING columnstore;
+INSERT INTO t (b) VALUES (1), (2);
+DROP TABLE t;
+
+CREATE TABLE t (a int GENERATED ALWAYS AS IDENTITY, b int) USING columnstore;
+INSERT INTO t (b) VALUES (1), (2);
+INSERT INTO t (b) SELECT 3;
+SELECT * FROM t;
+DROP TABLE t;
+
+CREATE TABLE t (a int GENERATED ALWAYS AS (b + 1) STORED, b int) USING columnstore;

commit f6ef7654f2429fdad6107730cbbb576216562259

Fix build error on PG15/16
Fix build error on PG15/16 caused by https://github.com/Mooncake-Labs/pg_mooncake/commit/6d34be55fc0a724a5fccf1a0c38e1f167b6a30fe

src/pgduckdb/catalog/pgduckdb_table.cpp
@@ -50,6 +50,7 @@ PostgresTable::OpenRelation(Oid relid) {
 void
 PostgresTable::SetTableInfo(CreateTableInfo &info, ::Relation rel) {
 	auto desc = RelationGetDescr(rel);
+	int defval_index = 0;
 
 	for (int i = 0; i < desc->natts; ++i) {
 		Form_pg_attribute attr = &desc->attrs[i];
@@ -57,7 +58,12 @@ PostgresTable::SetTableInfo(CreateTableInfo &info, ::Relation rel) {
 		auto duck_type = pgduckdb::ConvertPostgresToDuckColumnType(attr);
 		ColumnDefinition column(col_name, duck_type);
 		if (attr->atthasdef) {
-			Node *node = TupleDescGetDefault(desc, i + 1);
+			Assert(desc->constr);
+			Assert(desc->constr->defval);
+			Assert(defval_index < desc->constr->num_defval);
+			AttrDefault &defval = desc->constr->defval[defval_index++];
+			Assert(defval.adnum == i + 1);
+			Node *node = static_cast<Node *>(stringToNode(defval.adbin));
 			if (!IsA(node, Const)) {
 				elog(ERROR, "column \"%s\" has unsupported default value ", NameStr(attr->attname));
 			}
@@ -69,7 +75,11 @@ PostgresTable::SetTableInfo(CreateTableInfo &info, ::Relation rel) {
 				    pgduckdb::ConvertPostgresParameterToDuckValue(val->constvalue, val->consttype)));
 			}
 		} else if (attr->attidentity) {
+#if PG_VERSION_NUM >= 170000
 			Oid seqid = getIdentitySequence(rel, i + 1, false /*missing_ok*/);
+#else
+			Oid seqid = getIdentitySequence(RelationGetRelid(rel), i + 1, false /*missing_ok*/);
+#endif
 			vector<unique_ptr<ParsedExpression>> children;
 			children.push_back(make_uniq<ConstantExpression>(Value::UINTEGER(seqid)));
 			column.SetDefaultValue(make_uniq<FunctionExpression>("pg_nextval", std::move(children)));

test/expected/unsupported/merge.out
@@ -4,7 +4,6 @@ CREATE TABLE t (a int, b int) USING columnstore;
 INSERT INTO t VALUES (0), (1);
 MERGE INTO t USING s ON t.a = s.a
 WHEN MATCHED THEN UPDATE SET b = s.a + 1
-WHEN NOT MATCHED BY SOURCE THEN DELETE
-WHEN NOT MATCHED BY TARGET THEN INSERT VALUES (s.a, s.a + 1);
+WHEN NOT MATCHED THEN INSERT VALUES (s.a, s.a + 1);
 ERROR:  DuckDB does not support MERGE INTO statement
 DROP TABLE t, s;

test/sql/unsupported/merge.sql
@@ -6,7 +6,6 @@ INSERT INTO t VALUES (0), (1);
 
 MERGE INTO t USING s ON t.a = s.a
 WHEN MATCHED THEN UPDATE SET b = s.a + 1
-WHEN NOT MATCHED BY SOURCE THEN DELETE
-WHEN NOT MATCHED BY TARGET THEN INSERT VALUES (s.a, s.a + 1);
+WHEN NOT MATCHED THEN INSERT VALUES (s.a, s.a + 1);
 
 DROP TABLE t, s;
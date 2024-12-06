64edfb9e732c31e2dbd42ee79c4cb7c978a177b7
dentiny <dentinyhao@gmail.com>
2024-12-06 11:03:47 +0000

    update table name for consistency

--- test/sql/unsupported/types.sql
+++ test/sql/unsupported/types.sql
@@ -3,5 +3,5 @@ CREATE TABLE t (a jsonb) USING columnstore;
 CREATE TYPE point AS (x int, y int);
 CREATE TABLE t (a point) USING columnstore;
 
-CREATE TABLE mooncake_numeric_tbl (val numeric(40, 5)) USING columnstore;
-CREATE TABLE mooncake_numeric_tbl (val numeric) USING columnstore;
+CREATE TABLE t (val numeric(40, 5)) USING columnstore;
+CREATE TABLE t (val numeric) USING columnstore;

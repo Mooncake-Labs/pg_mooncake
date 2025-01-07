f69103ff4099dac3282b9175cc9b56af102fb981
nbiscaro <biscaronolan@gmail.com>
2025-01-07 22:06:17 +0000

    add new CTAS tests

--- test/sql/create_table_as.sql
+++ test/sql/create_table_as.sql
@@ -3,10 +3,20 @@ LOAD 'pg_mooncake';
 CREATE TABLE s (a int);
 INSERT INTO s VALUES (1), (2), (3);
 
+CREATE TABLE c (a int) USING columnstore;
+INSERT INTO c VALUES (1), (2), (3);
+
 CREATE TABLE t AS SELECT * FROM s;
 SELECT * FROM t;
 DROP TABLE t;
 
 CREATE TABLE t USING columnstore AS SELECT * FROM s;
+SELECT * FROM t;
+DROP TABLE t;
+
+CREATE TABLE t USING columnstore AS SELECT * FROM c;
+SELECT * FROM t;
+DROP TABLE t;
 
 DROP TABLE s;
+DROP TABLE c;

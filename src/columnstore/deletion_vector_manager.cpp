3b7aaad21874c12b59b7f72b3d934878d5ff2019
nbiscaro <biscaronolan@gmail.com>
2025-01-23 04:48:47 +0000

    add dvmanager helpers

--- src/columnstore/deletion_vector_manager.cpp
+++ src/columnstore/deletion_vector_manager.cpp
@@ -1,6 +1,5 @@
-// dv_manager.cpp
 #include "deletion_vector_manager.hpp"
-#include "parquet_reader.hpp"
+#include "filesystem"
 #include "pgduckdb/pgduckdb_utils.hpp"
 #include "pgmooncake_guc.hpp"
 
@@ -24,25 +23,40 @@ namespace duckdb {
 
 namespace {
 
-Datum StringGetTextDatum(const string &s) {
-    return PointerGetDatum(cstring_to_text_with_len(s.data(), s.size()));
+Datum StringGetTextDatum(const string_t &s) {
+    return PointerGetDatum(cstring_to_text_with_len(s.GetData(), s.GetSize()));
+}
+
+Datum StringGetUUIDDatum(const std::string &file_path) {
+    std::filesystem::path p(file_path);
+    const auto &uuid_str = p.stem().string();
+    return DirectFunctionCall1(uuid_in, CStringGetDatum(uuid_str.c_str()));
+}
+
+string_t TextDatumGetStringT(Datum d) {
+    auto validity_mask_text = PG_DETOAST_DATUM_PACKED(d);
+    uint32_t size = VARSIZE_ANY_EXHDR(validity_mask_text);
+    char *ptr = VARDATA_ANY(validity_mask_text);
+    return string_t(ptr, size);
 }
 
 Oid Mooncake() {
-    return get_namespace_oid("mooncake", false /*missing_ok*/);
+    return get_namespace_oid("mooncake", false);
 }
+
 static Oid DeletionVectors() {
     return get_relname_relid("deletion_vectors", Mooncake());
 }
+
 Oid DeletionVectorsFileGroupChunk() {
-    return get_relname_relid("deletion_vectors_file_chunk", Mooncake());
+    return get_relname_relid("deletion_vectors_uuid_chunk", Mooncake());
 }
 
 } // namespace
 
-void DVManager::UpsertDV(const std::string &file_name, uint64_t chunk_idx, const DeletionVector &deletion_vector) {
+void DVManager::UpsertDV(const std::string &file_path, uint64_t chunk_idx, const DeletionVector &deletion_vector) {
     UpsertDVOperation op;
-    op.file_name = file_name;
+    op.file_path = file_path;
     op.chunk_idx = chunk_idx;
     op.deletion_vector = deletion_vector;
 
@@ -52,28 +66,32 @@ void DVManager::UpsertDV(const std::string &file_name, uint64_t chunk_idx, const
     }
 }
 
-DeletionVector DVManager::FetchDV(const string &file_name, const uint64_t chunk_idx) {
+DeletionVector DVManager::FetchDV(const string &file_path, const uint64_t chunk_idx) {
     ::Relation table = table_open(DeletionVectors(), AccessShareLock);
     ::Relation index = index_open(DeletionVectorsFileGroupChunk(), AccessShareLock);
-    ScanKeyData key[2];
-    ScanKeyInit(&key[0], 1 /* attributeNumber */, BTEqualStrategyNumber, F_TEXTEQ, StringGetTextDatum(file_name));
-    ScanKeyInit(&key[1], 2 /* attributeNumber */, BTEqualStrategyNumber, F_INT8EQ, Int64GetDatum(chunk_idx));
-    SysScanDesc scan = systable_beginscan_ordered(table, index, snapshot, 2 /*nkeys*/, key);
 
-    string deletion_vector;
+    ScanKeyData key[2];
+    ScanKeyInit(&key[0], 1, BTEqualStrategyNumber, F_UUID_EQ, StringGetUUIDDatum(file_path));
+    ScanKeyInit(&key[1], 2, BTEqualStrategyNumber, F_INT8EQ, Int64GetDatum(chunk_idx));
+
+    SysScanDesc scan = systable_beginscan_ordered(table, index, snapshot, 2, key);
+    DeletionVector result;
     HeapTuple tuple;
     Datum values[x_deletion_vectors_natts];
     bool isnull[x_deletion_vectors_natts];
+
     if (HeapTupleIsValid(tuple = systable_getnext_ordered(scan, ForwardScanDirection))) {
         heap_deform_tuple(tuple, RelationGetDescr(table), values, isnull);
-        deletion_vector = TextDatumGetCString(values[2]);
+        if (!isnull[2]) {
+            string_t serialized_mask = TextDatumGetStringT(values[2]);
+            result = DeletionVector::Deserialize(serialized_mask);
+        }
     }
 
     systable_endscan_ordered(scan);
     index_close(index, AccessShareLock);
     table_close(table, AccessShareLock);
-
-    return DeletionVector::Deserialize(deletion_vector);
+    return result;
 }
 
 void DVManager::FlushUpsertDV() {
@@ -81,18 +99,18 @@ void DVManager::FlushUpsertDV() {
     ::Relation index = index_open(DeletionVectorsFileGroupChunk(), RowExclusiveLock);
 
     for (auto &op : upsert_operations_buffer) {
-        DeleteDV(op, table, index, snapshot);
-        InsertDV(op, table);
+        FlushDeleteDV(op, table, index, snapshot);
+        FlushInsertDV(op, table);
     }
 
     index_close(index, RowExclusiveLock);
     table_close(table, RowExclusiveLock);
 }
 
-void DVManager::DeleteDV(const UpsertDVOperation &op, ::Relation table, ::Relation index, Snapshot snapshot) {
+void DVManager::FlushDeleteDV(const UpsertDVOperation &op, ::Relation table, ::Relation index, Snapshot snapshot) {
     ScanKeyData key[2];
-    ScanKeyInit(&key[0], 1 /* attributeNumber */, BTEqualStrategyNumber, F_TEXTEQ, StringGetTextDatum(op.file_name));
-    ScanKeyInit(&key[1], 2 /* attributeNumber */, BTEqualStrategyNumber, F_INT8EQ, Int64GetDatum(op.chunk_idx));
+    ScanKeyInit(&key[0], 1, BTEqualStrategyNumber, F_UUID_EQ, StringGetUUIDDatum(op.file_path));
+    ScanKeyInit(&key[1], 2, BTEqualStrategyNumber, F_INT8EQ, Int64GetDatum(op.chunk_idx));
 
     SysScanDesc scan = systable_beginscan_ordered(table, index, snapshot, 2, key);
     HeapTuple tuple = systable_getnext_ordered(scan, ForwardScanDirection);
@@ -103,17 +121,76 @@ void DVManager::DeleteDV(const UpsertDVOperation &op, ::Relation table, ::Relati
     systable_endscan_ordered(scan);
 }
 
-void DVManager::InsertDV(const UpsertDVOperation &op, ::Relation table) {
+void DVManager::FlushInsertDV(UpsertDVOperation &op, ::Relation table) {
     bool nulls[x_deletion_vectors_natts] = {false, false, false};
-    Datum values[x_deletion_vectors_natts];
 
-    values[0] = StringGetTextDatum(op.file_name);
-    values[1] = Int64GetDatum(op.chunk_idx);
-    values[2] = StringGetTextDatum(DeletionVector::Serialize(op.deletion_vector));
+    MemoryStream write_stream;
+    DeletionVector::Serialize(write_stream, op.deletion_vector);
+    auto serialized_mask = string_t(reinterpret_cast<const char *>(write_stream.GetData()),
+                                    static_cast<uint32_t>(write_stream.GetPosition()));
+
+    Datum values[x_deletion_vectors_natts] = {StringGetUUIDDatum(op.file_path), Int64GetDatum(op.chunk_idx),
+                                              StringGetTextDatum(serialized_mask)};
 
     HeapTuple new_tuple = heap_form_tuple(RelationGetDescr(table), values, nulls);
     PostgresFunctionGuard(CatalogTupleInsert, table, new_tuple);
     CommandCounterIncrement();
+}
+
+void DVManager::ApplyDeletionVectors(const FileChunkDVMap &file_chunk_map, const vector<string> &file_paths) {
+    for (auto &file_entry : file_chunk_map) {
+        const auto file_number = file_entry.first;
+        const auto &file_path = file_paths[file_number];
+        const auto &chunk_map = file_entry.second;
+
+        for (auto &chunk_entry : chunk_map) {
+            auto chunk_idx = chunk_entry.first;
+            const auto &new_dv = chunk_entry.second;
+            DeletionVector old_dv = FetchDV(file_path, chunk_idx);
+            DeletionVector combined_dv = new_dv;
+            combined_dv.Combine(old_dv, old_dv.Size());
+            UpsertDV(file_path, chunk_idx, combined_dv);
+        }
+    }
+}
+
+FileChunkDVMap DVManager::BuildFileChunkDVs(const vector<row_t> &row_ids) {
+    FileChunkDVMap file_chunk_map;
+    for (const auto row_id : row_ids) {
+        auto file_number = int32_t(row_id >> 32);
+        auto offset_in_file = uint32_t(row_id & 0xFFFFFFFF);
+        idx_t chunk_idx = offset_in_file / STANDARD_VECTOR_SIZE;
+        idx_t offset_in_chunk = offset_in_file % STANDARD_VECTOR_SIZE;
+        file_chunk_map[file_number][chunk_idx].MarkDeleted(offset_in_chunk);
+    }
+    return file_chunk_map;
+}
+
+void DVManager::ReadAndAppendDeletedRows(ClientContext &context, ParquetReader &reader, const ChunkDVMap &chunk_dv_map,
+                                         const ColumnList &columns, ColumnDataCollection *return_collection) {
+    ParquetReaderScanState state;
+    DataChunk chunk;
+    chunk.Initialize(Allocator::Get(context), columns.GetColumnTypes());
+
+    vector<idx_t> groups_to_read(reader.GetFileMetadata()->row_groups.size());
+    reader.InitializeScan(context, state, std::move(groups_to_read));
+
+    for (auto &[chunk_idx, current_dv] : chunk_dv_map) {
+        idx_t current_chunk_idx = 0;
+        while (true) {
+            chunk.Reset();
+            reader.Scan(state, chunk);
+            if (chunk.size() == 0) {
+                break;
+            }
+            if (current_chunk_idx == chunk_idx) {
+                current_dv.ApplyToChunk(chunk);
+                return_collection->Append(chunk);
+                break;
+            }
+            current_chunk_idx++;
+        }
+    }
 }
 
 } // namespace duckdb
--- src/columnstore/deletion_vector_manager.hpp
+++ src/columnstore/deletion_vector_manager.hpp
@@ -3,15 +3,19 @@
 #include "columnstore_deletion_vector.hpp"
 #include "duckdb/common/vector.hpp"
 #include "duckdb/storage/storage_info.hpp"
+#include "parquet_reader.hpp"
 #include "pgduckdb/pg/declarations.hpp"
 
 namespace duckdb {
+
+using ChunkDVMap = std::unordered_map<idx_t, DeletionVector>;
+using FileChunkDVMap = std::unordered_map<idx_t, ChunkDVMap>;
 
 constexpr int x_deletion_vectors_natts = 3;
 constexpr int x_op_batch_size = duckdb::Storage::ROW_GROUP_SIZE;
 
 struct UpsertDVOperation {
-    string file_name;
+    string file_path;
     uint64_t chunk_idx;
     DeletionVector deletion_vector;
 };
@@ -20,22 +24,28 @@ class DVManager {
 public:
     explicit DVManager(Snapshot snapshot) : snapshot(snapshot) {}
 
-    void UpsertDV(const std::string &file_name, uint64_t chunk_idx, const DeletionVector &deletion_vector);
+    void UpsertDV(const std::string &file_path, uint64_t chunk_idx, const DeletionVector &deletion_vector);
+    DeletionVector FetchDV(const std::string &file_path, uint64_t chunk_idx);
 
-    DeletionVector FetchDV(const std::string &file_name, uint64_t chunk_idx);
+    void ApplyDeletionVectors(const FileChunkDVMap &file_chunk_map, const vector<string> &file_paths);
 
-    void Flush() {
+    static FileChunkDVMap BuildFileChunkDVs(const vector<row_t> &row_ids);
+
+    static void ReadAndAppendDeletedRows(ClientContext &context, ParquetReader &reader, const ChunkDVMap &chunk_dv_map,
+                                         const ColumnList &columns, ColumnDataCollection *return_collection);
+
+    inline void Flush() {
         FlushUpsertDV();
         upsert_operations_buffer.clear();
-    };
+    }
 
 private:
     Snapshot snapshot;
     std::vector<UpsertDVOperation> upsert_operations_buffer;
 
     void FlushUpsertDV();
-    void DeleteDV(const UpsertDVOperation &op, ::Relation table, ::Relation index, Snapshot snapshot);
-    void InsertDV(const UpsertDVOperation &op, ::Relation table);
+    void FlushDeleteDV(const UpsertDVOperation &op, ::Relation table, ::Relation index, Snapshot snapshot);
+    void FlushInsertDV(UpsertDVOperation &op, ::Relation table);
 };
 
 } // namespace duckdb

ecb1455707119e27b0e0ddf3741e63bf67fda1d1
Cheng Chen <cheng@mooncakelabs.com>
2025-02-11 01:38:15 +0000

    Run initdb at devcontainer startup

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,7 @@
 git config --global --add safe.directory /workspaces/pg_mooncake
 git config --global --add safe.directory /workspaces/pg_mooncake/third_party/duckdb
 
+rm -rf /usr/local/pgsql/data
+initdb -D /usr/local/pgsql/data
 pg_ctl -D /usr/local/pgsql/data start
-dropdb --if-exists mooncake
 createdb mooncake

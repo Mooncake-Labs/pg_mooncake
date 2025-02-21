a5dbfd686bbb7c80b2ba7397482c3212271bbdf3
raghunandanbhat <raghunandan.bhat96@gmail.com>
2025-02-21 09:40:09 +0530

    fix tablespace test

--- test/sql/tablespace.sql
+++ test/sql/tablespace.sql
@@ -1,5 +1,5 @@
 DROP TABLESPACE IF EXISTS mooncake_ts;
-CREATE TABLESPACE mooncake_ts LOCATION '/home/postgres/tablespace_dir';
+CREATE TABLESPACE mooncake_ts LOCATION '/tmp/tablespace_dir';
 CREATE TABLE t (a int) USING columnstore TABLESPACE mooncake_ts;
 SELECT table_name, regexp_replace(path, '[0-9]+/$', 'XXXX/') AS path FROM mooncake.columnstore_tables;
 DROP TABLE t;

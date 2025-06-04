03a700fb892c83fef745ea0245d764878f9cddfa
Cheng Chen <cheng@mooncakelabs.com>
2025-06-04 06:44:40 +0000

    Handle closed connection

--- src/pgmoonlink/server.rs
+++ src/pgmoonlink/server.rs
@@ -28,39 +28,44 @@ pub(super) async fn start() {
     }
 }
 
-async fn handle_stream(mut stream: UnixStream) {
+async fn handle_stream(mut stream: UnixStream) -> Result<(), Eof> {
     let mut map = HashMap::new();
     loop {
-        match read(&mut stream).await {
+        match read(&mut stream).await? {
             Request::CreateSnapshot { table_id, lsn } => {
-                create_snapshot(&mut stream, table_id, lsn).await
+                create_snapshot(&mut stream, table_id, lsn).await?
             }
             Request::CreateTable {
                 table_id,
                 uri,
                 table,
-            } => create_table(&mut stream, table_id, table, uri).await,
+            } => create_table(&mut stream, table_id, table, uri).await?,
             Request::ScanTableBegin { table_id, lsn } => {
-                scan_table_begin(&mut stream, &mut map, table_id, lsn).await
+                scan_table_begin(&mut stream, &mut map, table_id, lsn).await?
             }
             Request::ScanTableEnd { table_id } => {
-                scan_table_end(&mut stream, &mut map, table_id).await
+                scan_table_end(&mut stream, &mut map, table_id).await?
             }
         }
     }
 }
 
-async fn create_snapshot(stream: &mut UnixStream, table_id: TableId, lsn: u64) {
+async fn create_snapshot(stream: &mut UnixStream, table_id: TableId, lsn: u64) -> Result<(), Eof> {
     BACKEND
         .create_iceberg_snapshot(&table_id, lsn)
         .await
         .unwrap();
-    write(stream, &()).await;
+    write(stream, &()).await
 }
 
-async fn create_table(stream: &mut UnixStream, table_id: TableId, table: String, uri: String) {
+async fn create_table(
+    stream: &mut UnixStream,
+    table_id: TableId,
+    table: String,
+    uri: String,
+) -> Result<(), Eof> {
     BACKEND.create_table(table_id, &table, &uri).await.unwrap();
-    write(stream, &()).await;
+    write(stream, &()).await
 }
 
 async fn scan_table_begin(
@@ -68,35 +73,48 @@ async fn scan_table_begin(
     map: &mut HashMap<TableId, Arc<ReadState>>,
     table_id: TableId,
     lsn: u64,
-) {
+) -> Result<(), Eof> {
     let state = BACKEND.scan_table(&table_id, Some(lsn)).await.unwrap();
-    write(stream, &state.data).await;
+    write(stream, &state.data).await?;
     map.insert(table_id, state);
+    Ok(())
 }
 
 async fn scan_table_end(
     stream: &mut UnixStream,
     map: &mut HashMap<TableId, Arc<ReadState>>,
     table_id: TableId,
-) {
+) -> Result<(), Eof> {
     map.remove(&table_id);
-    write(stream, &()).await;
+    write(stream, &()).await
 }
 
-async fn write<E: Encode>(stream: &mut UnixStream, data: &E) {
+async fn write<E: Encode>(stream: &mut UnixStream, data: &E) -> Result<(), Eof> {
     let bytes = bincode::encode_to_vec(data, BINCODE_CONFIG).unwrap();
     let len = u32::try_from(bytes.len()).unwrap();
-    stream.write_all(&len.to_ne_bytes()).await.unwrap();
-    stream.write_all(&bytes).await.unwrap();
+    check_eof(stream.write_all(&len.to_ne_bytes()).await)?;
+    check_eof(stream.write_all(&bytes).await)
 }
 
-async fn read<D: Decode<()>>(stream: &mut UnixStream) -> D {
+async fn read<D: Decode<()>>(stream: &mut UnixStream) -> Result<D, Eof> {
     let mut buf = [0; 4];
-    stream.read_exact(&mut buf).await.unwrap();
+    check_eof(stream.read_exact(&mut buf).await)?;
     let len = u32::from_ne_bytes(buf);
     let mut bytes = vec![0; len as usize];
-    stream.read_exact(&mut bytes).await.unwrap();
-    bincode::decode_from_slice(&bytes, BINCODE_CONFIG)
-        .unwrap()
-        .0
+    check_eof(stream.read_exact(&mut bytes).await)?;
+    let (data, _) = bincode::decode_from_slice(&bytes, BINCODE_CONFIG).unwrap();
+    Ok(data)
+}
+
+struct Eof;
+
+fn check_eof<T>(r: std::io::Result<T>) -> Result<T, Eof> {
+    use std::io::ErrorKind::*;
+    match r {
+        Ok(t) => Ok(t),
+        Err(e) => match e.kind() {
+            BrokenPipe | ConnectionReset | UnexpectedEof => Err(Eof),
+            _ => panic!("IO error: {e}"),
+        },
+    }
 }
